eaa135e62d56323cd302b70c86562bc2ff67e0cb
neonbulb <38469401+neonbulb@users.noreply.github.com>
2023-02-06 14:48:34 -0500

    Add javascript exercise file

---
+++ use-destructuring-assignment-to-extract-values-from-objects.js
@@ -0,0 +1,14 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+Object.freeze(HIGH_TEMPERATURES);
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+console.log(today, tomorrow);
+
+// Only change code above this line
