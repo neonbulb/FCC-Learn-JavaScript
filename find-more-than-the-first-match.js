0755f0d2390df7281f9c8c58d300e00b85a982d6
neonbulb <38469401+neonbulb@users.noreply.github.com>
2023-02-08 14:11:59 -0500

    Add javascript exercise file

---
+++ find-more-than-the-first-match.js
@@ -0,0 +1,5 @@
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+
+console.log(result);
