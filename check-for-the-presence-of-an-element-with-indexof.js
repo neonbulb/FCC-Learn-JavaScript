d59643ee7b0b54c3c8eacf03a120448e6f0630fa
neonbulb <38469401+neonbulb@users.noreply.github.com>
2023-02-12 22:09:32 -0500

    Add javascript exercise file

---
+++ check-for-the-presence-of-an-element-with-indexof.js
@@ -0,0 +1,10 @@
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  const index = arr.indexOf(elem);
+  return index == -1 ? false : true;
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+
