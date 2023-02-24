b674961524e32cb6f9498b13926164756a46d99c
neonbulb <38469401+neonbulb@users.noreply.github.com>
2023-02-24 13:19:38 -0500

    Add javascript exercise file

---
+++ apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function urlSlug(title) {
+
+  return title.trim().split(/\s+/).join("-").toLowerCase();
+
+}
+// Only change code above this line
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+console.log(urlSlug(" Winter Is   Coming"));
